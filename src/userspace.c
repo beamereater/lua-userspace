e038aff0816df5638bf14f2f1251a735af393f63
beamereater <beamerjajaja@gmail.com>
2025-08-04 22:32:36 -0400

    Change binary name from lua to lua-userspace

--- src/userspace.c
+++ src/userspace.c
@@ -1,10 +1,18 @@
 #include "lua.h"
 #include "lauxlib.h"
+#include <dirent.h>
 #include <unistd.h>
 
 int lclear(lua_State *l) {
 	write(1,"\033[H\033[2J",8);
 	return 0;
+}
+
+int lopendir(lua_State *l) {
+	luaL_checkstring(l,1);
+	DIR* fd = opendir(lua_tostring(l,1));
+	lua_pushnumber(l,fd);
+	return 1;
 }
 
 const struct luaL_Reg func[] = {
